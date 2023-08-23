23b2b106fdd86e59446f1e5197b5ebfe82b7c06b
Lucas M. Lacerda <82623575+PortalSoap@users.noreply.github.com>
2023-08-23 14:39:44 -0300

    Adição inicial de arquivos
    Os arquivos essenciais para o funcionamento da página foram adicionados.

---
+++ index.js
@@ -0,0 +1,7 @@
+function showDateAndTime() {
+    document.getElementById("date-and-time").innerHTML = Date();
+}
+
+function showHelloWorld() {
+    document.getElementById("hello-world").innerHTML = "Olá Mundo!";
+}
